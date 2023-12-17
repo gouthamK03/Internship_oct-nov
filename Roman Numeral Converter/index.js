9b9bb5a47369cf3370c86cd9bceb5d2ba0adc181
gouthamK03 <132644175+gouthamK03@users.noreply.github.com>
2023-12-17 15:44:22 +0530

    index.js

--- Roman Numeral Converter/index.js
+++ Roman Numeral Converter/index.js
@@ -1,43 +1,4 @@
 
-Only you can see this message. Congratulations on earning this certification. It's no easy task. Running freeCodeCamp isn't easy either. Nor is it cheap. Help us help you and many other people around the world. Make a tax-deductible supporting donation to our charity today.
-
-Confirm your donation of $20 / month:
-$5
-$10
-$20
-$40
-Your $20 donation will provide 1,000 hours of learning to people around the world each month.
-
-Donate
-This certifies that
-K GOUTHAM
-successfully completed the
-JavaScript Algorithms and Data Structures
-Developer Certification on December 15, 2023
-representing approximately 300 hours of work
-Quincy Larson's Signature
-Quincy Larson
-
-Executive Director, freeCodeCamp.org
-
-
-Verify this certification at:
-https://freecodecamp.org/certification/fcc1e1dc437-7a9e-4ad2-99a5-bcc0c274b1b6/javascript-algorithms-and-data-structures
-
-Add this certification to my LinkedIn profile
-Share this certification on Twitter
-As part of this certification, K GOUTHAM built the following projects and got all automated test suites to pass:
-Certifications
-Palindrome Checker	
-Roman Numeral Converter	
-Caesars Cipher	
-Telephone Number Validator	
-Cash Register	
-If you suspect that any of these projects violate the academic honesty policy, please report this to our team.
-Navigated to JavaScript Algorithms and Data Structures Projects: Roman Numeral Converter | freeCodeCamp.org
-×Close
-Solution for Roman Numeral Converter
-JS
 function convertToRoman(num) {
 //Break the number into Thousands, Hundreds, Tens and Ones, and write down each in turn.
 //get the digits
@@ -56,6 +17,9 @@ const lookup=[
     // the given above symbols for the respected value
 return lookup[3][Thousands]+lookup[2][Hundreds]+
 lookup[1][Tens]+lookup[0][Ones];
+}
+
+convertToRoman(36);
 }
 
 convertToRoman(36);
